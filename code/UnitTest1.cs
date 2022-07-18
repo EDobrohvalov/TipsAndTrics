9d538a9bfabb0ee8ea97c46e1b93e367b1825ef6
Eugene Dobrokhvalov <evgeniy.dobrokhvalov@geonaft.ai>
2022-07-19 00:44:37 +0300

    add presentation about Either

---
+++ code/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System.Text;
+
+namespace code;
+
+public class WorkshopGoto
+{
+    public void SomeMethod()
+    {
+
+    }
+
+    public void AnotherMethod()
+    {
+
+    }
+}
+
+public class EitherTest
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
